afddb192a6e12a71a8360e06f4da66b3020ba49f
norang <norang1221@gmail.com>
2025-01-09 16:25:40 +0900

    에셋 및 기본 파일 추가

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var coverImageView: UIImageView!
+    @IBOutlet var collectionView: UICollectionView!
+    @IBOutlet var inputTextField: UITextField!
+    @IBOutlet var resultButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ UpDownGame/numberCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  numberCollectionViewCell.swift
+//  UpDownGame
+//
+//  Created by Kyuhee hong on 1/9/25.
+//
+
+import UIKit
+
+class numberCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
